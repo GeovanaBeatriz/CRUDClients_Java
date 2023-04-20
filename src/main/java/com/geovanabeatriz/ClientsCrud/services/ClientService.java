a679c873b7aa72705d4227b5499f2ac5ee8757b0
Geovana Beatriz <geovana.jesus.184945@gmail.com>
2023-04-20 16:39:18 -0300

    Delete Clientes com Delete

--- src/main/java/com/geovanabeatriz/ClientsCrud/services/ClientService.java
+++ src/main/java/com/geovanabeatriz/ClientsCrud/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.geovanabeatriz.ClientsCrud.dto.ClientDTO;
 import com.geovanabeatriz.ClientsCrud.entities.Client;
 import com.geovanabeatriz.ClientsCrud.repositories.ClientRepository;
+import com.geovanabeatriz.ClientsCrud.services.exceptions.DatabaseException;
 import com.geovanabeatriz.ClientsCrud.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -67,5 +70,17 @@ public class ClientService {
 			catch(EntityNotFoundException e) {
 				throw new ResourceNotFoundException("id not found");
 			}
+	}
+	
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id); 
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation"); 
+		}
 	}
 }
