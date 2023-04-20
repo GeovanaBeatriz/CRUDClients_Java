8d371a4525fb01135efdc1c37067c1107294e3f0
Geovana Beatriz <geovana.jesus.184945@gmail.com>
2023-04-20 12:24:59 -0300

    Client DTO

--- src/main/java/com/geovanabeatriz/ClientsCrud/resources/ClientResource.java
+++ src/main/java/com/geovanabeatriz/ClientsCrud/resources/ClientResource.java
@@ -1,7 +1,5 @@
 package com.geovanabeatriz.ClientsCrud.resources;
 
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.geovanabeatriz.ClientsCrud.entities.Client;
+import com.geovanabeatriz.ClientsCrud.dto.ClientDTO;
 import com.geovanabeatriz.ClientsCrud.services.ClientService;
 
 @RestController
@@ -21,11 +19,11 @@ public class ClientResource {
 	private ClientService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
+	public ResponseEntity<List<ClientDTO>> findAll(){
 		//1 - List<Client> list = new ArrayList<>();
 		//1- list.add(new Client(1L, "Maria Sousa", "51810490871", 5.80, Instant.now(), 5));
 		
-		List<Client> list = service.findAll();
+		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/geovanabeatriz/ClientsCrud/services/ClientService.java
+++ src/main/java/com/geovanabeatriz/ClientsCrud/services/ClientService.java
@@ -1,10 +1,15 @@
 package com.geovanabeatriz.ClientsCrud.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale.Category;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.geovanabeatriz.ClientsCrud.dto.ClientDTO;
 import com.geovanabeatriz.ClientsCrud.entities.Client;
 import com.geovanabeatriz.ClientsCrud.repositories.ClientRepository;
 
@@ -14,7 +19,15 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public List<Client> findAll(){
-		return repository.findAll();
+	
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll(); //tem q transformar isso em DTO agora
+		
+		List<ClientDTO> listDto = new ArrayList<>();
+		for(Client cat : list) {
+			listDto.add(new ClientDTO(cat));
+		} //Convertendo em dto
+		
+		return listDto;
 	}
 }
