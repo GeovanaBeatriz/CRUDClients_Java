da774803a14ea1e7ea1f1f4eda58b15d391fac0f
Geovana Beatriz <geovana.jesus.184945@gmail.com>
2023-04-20 16:24:47 -0300

    Update Clientes com PUT

--- src/main/java/com/geovanabeatriz/ClientsCrud/services/ClientService.java
+++ src/main/java/com/geovanabeatriz/ClientsCrud/services/ClientService.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.geovanabeatriz.ClientsCrud.dto.ClientDTO;
 import com.geovanabeatriz.ClientsCrud.entities.Client;
@@ -18,7 +21,7 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	
+	@Transactional(readOnly = true)
 	public List<ClientDTO> findAll(){
 		List<Client> list = repository.findAll(); //tem q transformar isso em DTO agora
 		
@@ -30,13 +33,14 @@ public class ClientService {
 		return listDto;
 	}
 	
-	
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id); //puxa do banco de dados
 		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found")); //Não achou id
 		return new ClientDTO(entity);
 	}
 	
+	@Transactional
 	public ClientDTO insert (ClientDTO dto) {
 		 Client entity = new Client();
 		 entity.setName(dto.getName());
@@ -46,5 +50,22 @@ public class ClientService {
 		 entity.setChildren(dto.getChildren());
 		 entity = repository.save(entity);
 		 return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id); //aqui não mexe no banco de dados ainda
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity); //somente aqui
+			return new ClientDTO(entity);
+			}
+			catch(EntityNotFoundException e) {
+				throw new ResourceNotFoundException("id not found");
+			}
 	}
 }
